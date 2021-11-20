349b86156f4aaaae26c135a5110a3f1b25f0d741
valli180 <67402528+valli180@users.noreply.github.com>
2021-11-19 19:54:42 -0800

    Commit adding main

--- demo.R
+++ demo.R
@@ -13,6 +13,11 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+main <- function(){
+  print(opt)
+  print(typeof(opt))
+  print(opt$arg4)
+}
+
+main()
+
